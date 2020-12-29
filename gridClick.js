d959bcdd4e34484eeafc8ef4b67e57c587aa5aeb
Adir <42638312+Adir-SL@users.noreply.github.com>
2020-12-29 10:13:50 +0200

    added swipe action

--- gridClick.js
+++ gridClick.js
@@ -1,8 +1,8 @@
 document.addEventListener("click", myFunction);
+// document.addEventListener("touchstart", startFunc);
 
 function myFunction(e) {
   if(e.target.tagName == "img" || e.target.tagName == "IMG" && e.target.id !== "chenIcon"){
-      console.log(e.target.src)
       document.getElementById("bigInner").style.backgroundImage = "url("+e.target.src+")";
       document.getElementById("bigImage").style.opacity = "1";
       document.getElementById("bigImage").style.pointerEvents = "all";
@@ -12,3 +12,25 @@ function myFunction(e) {
     document.getElementById("bigImage").style.pointerEvents = "none";
   }
 }
+
+// function startFunc(e){
+//   console.log(e.touchs[0].clientX);
+// }
+document.getElementById("bigImage").addEventListener('touchstart', function(e) {
+  clientX = e.touches[0].clientX;
+}, false);
+
+document.getElementById("bigImage").addEventListener('touchend', function(e) {
+  // Cache the client X/Y coordinates
+  deltaX = e.changedTouches[0].clientX - clientX;
+  temp = document.getElementById("bigInner").style.backgroundImage;
+  tempBef = temp.slice(0,temp.lastIndexOf("_")+1);
+  tempAft = temp.slice(temp.lastIndexOf("."), temp.length);
+  temp = temp.slice(temp.lastIndexOf("_")+1, temp.lastIndexOf("."));
+  temp = Number(temp);
+  if(deltaX > 0){
+    document.getElementById("bigInner").style.backgroundImage = tempBef+(Number(temp)+1)+tempAft;
+  }else{
+    document.getElementById("bigInner").style.backgroundImage = tempBef+(Number(temp)-1)+tempAft;
+  }
+}, false);
--- header.js
+++ header.js
@@ -1,5 +1,6 @@
 origBody = document.body.innerHTML;
 document.body.innerHTML = "<header><a href='index.html'><img src='img/favicon.png' id='chenIcon' /></a><a href='cutouts.html'>cutouts</a><a href='animations.html'>animations</a><a href='projects.html'>projects</a></header>" + origBody;
+document.body.innerHTML += "<footer><a href='https://www.facebook.com/chen.lev.98/' target='_blank'><img src='img/facebook.svg' /></a><a href='https://www.instagram.com/chen_lev_on_paper/' target='_blank'><img src='img/instagram.svg' /></a></footer>"
 
 var x = document.getElementsByTagName("header")[0].getElementsByTagName("a");
 var i;
